ea2ba26544852e203caf496a2dd7ec6a2ff7febf
Ekaterina Loshchinina <KateLoschinina@ekaterinasmbp6.home>
2017-03-11 13:22:03 +0000

    First requirement:
    When your server receives a request on http://localhost:4000/set?somekey=somevalue it should store the passed key and value in memory.
    
    Storing URL parameters in session.

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-var express = require('express');
-var app = express();
+var express = require('express')
+var app = express()
 
-app.get('/', function(req, res) {
-  res.send("Hey, world! How is it?");
-});
+app.get('/set', function (req, res) {
+  req.session = req.query
+  console.log(req.session.somekey)
+  res.send(req.session)
+})
 
 app.listen(3000, function() {
-  console.log('App is listening on 3000');
-});
+  console.log('App is listening on 3000')
+})
