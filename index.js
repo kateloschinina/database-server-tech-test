b9a3ab86d35803791f4822223658ad62965cba9b
Ekaterina Loshchinina <KateLoschinina@Ekaterinas-MacBook-Pro-6.local>
2017-03-11 16:19:55 +0000

    Was not able to make sessions working.
    Instead created a program that is storing meaning in variable. Works taking into account future implementation of databse.
    Proved that express technology is working for this purpose. Therefore next step would be to realise testing environment for it.
     Please enter the commit message for your changes. Lines starting

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 var express = require('express')
 var app = express()
-var session = require('express-session');
-var RedisStore = require('connect-redis')(session);
 
-app.use(session({ store: new RedisStore(), secret: '' }));
+var ses;
 
 app.get('/set', function (req, res) {
-  req.session = req.query
-  console.log(req.session.somekey)
-  res.send(req.session)
+  ses = req.query
+  res.send('The message has been sent. Please go to http://localhost:4000/get?key=somekey to check whether is was recieved.')
+})
+
+app.get('/get', function (req, res) {
+  if (ses[req.query.key]) {
+    res.send('Message was recieved')
+  } else {
+    res.send('Recheck url parametrs')
+  }
 })
 
 app.listen(3000, function() {
