0e843c62e432ebb1263449a0b46aabd7688e5acc
Ekaterina Loshchinina <KateLoschinina@Ekaterinas-MacBook-Pro-6.local>
2017-03-11 17:40:56 +0000

    Completed TDD for User Story 0:
    Write a program that runs a server that is accessible on http://localhost:4000/.
    
    Test passes.

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ var app = express()
 
 var ses;
 
+app.get('/', function (req, res) {
+  res.send('This is a landing page')
+})
+
 app.get('/set', function (req, res) {
   ses = req.query
-  res.send('The message has been sent. Please go to http://localhost:4000/get?key=somekey to check whether is was recieved.')
+  res.send('The message has been sent. Please go to http://localhost:4000/get?key=somekey to check whether is was recieved')
 })
 
 app.get('/get', function (req, res) {
--- test/test.js
+++ test/test.js
@@ -1,8 +1,31 @@
 var express = require('express')
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal(-1, [1,2,3].indexOf(4));
-    });
+var app = express()
+var request = require('supertest')
+
+// User Story 0
+//
+// Write a program that runs a server that is accessible on http://localhost:4000/
+
+describe('User story 0', function() {
+  it('should return a server that is accessible on http://localhost:4000/', function(done) {
+
+    app.get('/', function (req, res) {
+      res.send('This is a landing page')
+    })
+
+    request(app)
+    .get('/')
+    .expect('This is a landing page', done)
   });
 })
+
+
+// User Story 1
+//
+// When your server receives a request on http://localhost:4000/set?somekey=somevalue it should store the passed key and value in memory
+
+
+
+// User Story 2
+//
+// When it receives a request on http://localhost:4000/get?key=somekey it should return the value stored at somekey
